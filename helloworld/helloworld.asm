66d365cb7746e92664e4560793f7610b19a516d9
StatesideCash <bcampbell1120@gmail.com>
2016-02-13 20:13:50 -0800

    Hello world and intial makefile

---
+++ helloworld/helloworld.asm
@@ -0,0 +1,28 @@
+	.file	"helloworld.c"
+	.intel_syntax noprefix
+	.section	.rodata
+.LC0:
+	.string	"Hello reversing!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	edi, OFFSET FLAT:.LC0
+	mov	eax, 0
+	call	printf
+	mov	eax, 0
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.9.2-10ubuntu13) 4.9.2"
+	.section	.note.GNU-stack,"",@progbits
